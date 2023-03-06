a43e935a92c3bd65c4fbf4543dafc9d19156e9fa
Asad javed <ajaved01@teresol.com>
2023-03-06 17:02:27 +0500

    VueJs Practice

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div id="app">
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HomeTest data='Asad javed is here' msg=" Hy How are you"/>
+</div>
+</template>
+
+<script>
+import HomeTest from './components/HomeTest.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HomeTest
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HomeTest.vue
@@ -0,0 +1,34 @@
+<template>
+   <div>
+ <h1 v-if="show">hello World</h1>
+ <!-- <h1 v-else>hello World Block</h1> -->
+
+ <h2> {{data}}</h2>
+ <!-- <h2> {{msg}}</h2> -->
+ <button v-on:click="display">Call hello Function</button>
+ <button v-on:click="display">Toggle Button</button>
+   </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'HomeTest',
+    props: {
+        data:{
+            type: String,
+        },
+        msg: {
+            type:String,
+        }
+    },methods:{
+       //hello function
+        display(){
+            this.show=false
+        }
+    },data(){
+        return {show:true}
+    }
+}
+</script>
